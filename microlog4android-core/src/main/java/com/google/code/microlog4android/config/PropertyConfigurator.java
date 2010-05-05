392f82cc41f7d0655f4d37842c83ecc605e3079e
Johan Karlsson <johan.karlsson.malmo@gmail.com>
2010-05-05 22:49:09 +0200

    Changed the PropertyConfigurator

--- microlog4android-core/src/main/java/com/google/code/microlog4android/config/PropertyConfigurator.java
+++ microlog4android-core/src/main/java/com/google/code/microlog4android/config/PropertyConfigurator.java
@@ -17,10 +17,19 @@ import android.content.res.Resources;
 import android.content.res.Resources.NotFoundException;
 
 import com.google.code.microlog4android.Appender;
+import com.google.code.microlog4android.Formatter;
 import com.google.code.microlog4android.Level;
 import com.google.code.microlog4android.Logger;
 
 /**
+ * The {@link PropertyConfigurator} is used for configuration via a properties
+ * file. The properties file should be put in one of the following directories:
+ * 
+ * <ol>
+ * <li>The assets directory
+ * <li/>
+ * <li>The res/raw directory</li>
+ * </ol>
  * 
  * @author Johan Karlsson
  */
@@ -32,6 +41,8 @@ public class PropertyConfigurator {
 	 * The key for setting the root logger.
 	 */
 	public static final String ROOT_LOGGER_KEY = "microlog.rootLogger";
+
+	public static final String MICROLOG_PREFIX = "microlog";
 
 	/**
 	 * The key for setting the logger.
@@ -59,25 +70,38 @@ public class PropertyConfigurator {
 	public static final String TAG_PREFIX_KEY = "microlog.tag";
 
 	public static final String[] APPENDER_ALIASES = { "LogCatAppender",
-			"ConsoleAppender", "FileAppender" };
+			"FileAppender" };
 
 	public static final String[] APPENDER_CLASS_NAMES = {
 			"com.google.code.microlog4android.appender.LogCatAppender",
-			"com.google.code.microlog4android.appender.ConsoleAppender",
 			"com.google.code.microlog4android.appender.FileAppender" };
+
+	public static final String[] FORMATTER_ALIASES = { "SimpleFormatter",
+			"PatternFormatter" };
+
+	public static final String[] FORMATTER_CLASS_NAMES = {
+			"com.google.code.microlog4android.format.SimpleFormatter",
+			"com.google.code.microlog4android.format.PatternFormatter" };
 
 	private static final HashMap<String, String> appenderAliases = new HashMap<String, String>(
 			43);
+
+	private static final HashMap<String, String> formatterAliases = new HashMap<String, String>(
+			21);
 
 	private Context context;
 
 	private LoggerRepository loggerRepository;
 
-	// Static initializer
 	{
 		for (int index = 0; index < APPENDER_ALIASES.length; index++) {
 			appenderAliases.put(APPENDER_ALIASES[index],
 					APPENDER_CLASS_NAMES[index]);
+		}
+
+		for (int index = 0; index < FORMATTER_ALIASES.length; index++) {
+			formatterAliases.put(FORMATTER_ALIASES[index],
+					FORMATTER_CLASS_NAMES[index]);
 		}
 	};
 
@@ -102,12 +126,19 @@ public class PropertyConfigurator {
 	}
 
 	/**
-	 * Configure using the
+	 * Configure using the default properties filename, i.e
+	 * "microlog.properties".
 	 */
 	public void configure() {
 		configure(DEFAULT_PROPERTIES_FILENAME);
 	}
 
+	/**
+	 * Configure using the specified filename.
+	 * 
+	 * @param filename
+	 *            the filename of the properties file used for configuration.
+	 */
 	public void configure(String filename) {
 		Resources resources = context.getResources();
 		AssetManager assetManager = resources.getAssets();
@@ -122,6 +153,12 @@ public class PropertyConfigurator {
 		}
 	}
 
+	/**
+	 * Configure using the specified resource Id.
+	 * 
+	 * @param resId
+	 *            the resource Id where to load the properties from.
+	 */
 	public void configure(int resId) {
 		Resources resources = context.getResources();
 
@@ -178,7 +215,7 @@ public class PropertyConfigurator {
 		setLevel(properties);
 
 		String appenderString = properties.getProperty(
-				PropertyConfigurator.APPENDER_PREFIX_KEY, "ConsoleAppender");
+				PropertyConfigurator.APPENDER_PREFIX_KEY, "LogCatAppender");
 		List<String> appenderList = parseAppenderString(appenderString);
 		setAppenders(appenderList);
 
@@ -241,12 +278,44 @@ public class PropertyConfigurator {
 			System.err.println("No access to appender class: " + e);
 		} catch (InstantiationException e) {
 			System.err.println("Failed to instantiate appender class: " + e);
+		} catch (ClassCastException e) {
+			System.err
+					.println("Specified appender class does not implement the Appender interface: "
+							+ e);
 		}
 	}
 
 	private void setFormatter(Properties properties) {
-		// TODO Auto-generated method stub
 
+		String formatterString = (String) properties.getProperty(
+				FORMATTER_PREFIX_KEY, "PatternFormatter");
+
+		String className = null;
+
+		if (formatterString != null) {
+			className = formatterAliases.get(formatterString);
+		}
+
+		if (className == null) {
+			className = formatterString;
+		}
+
+		try {
+			Class formatterClass = Class.forName(className);
+			Formatter formatter = (Formatter) formatterClass.newInstance();
+		} catch (ClassNotFoundException e) {
+			System.err.println("Failed to find Formatter class: " + e);
+		} catch (InstantiationException e) {
+			System.err.println("Failed to instantiate formtter: " + e);
+		} catch (IllegalAccessException e) {
+			System.err.println("No access to formatter class: " + e);
+		} catch (ClassCastException e) {
+			System.err
+					.println("Specified formatter class does not implement the Formatter interface: "
+							+ e);
+		}
+
+		// TODO Add the formatter to the appender(s)
 	}
 
 	/**
