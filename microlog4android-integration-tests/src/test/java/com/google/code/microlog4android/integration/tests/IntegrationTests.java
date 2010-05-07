c3b2cf7cf49c8264158334fe8461d6cf4ed0a946
Jarle Hansen <=>
2010-05-07 21:38:41 +0200

    updates on the integration test project

---
+++ microlog4android-integration-tests/src/test/java/com/google/code/microlog4android/integration/tests/IntegrationTests.java
@@ -0,0 +1,11 @@
+package com.google.code.microlog4android.integration.tests;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class IntegrationTests extends TestCase {
+    
+	public void testSomething() {
+		Assert.assertEquals(2, 2);
+	}
+}
