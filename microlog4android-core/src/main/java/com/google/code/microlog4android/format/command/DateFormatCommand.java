400f6e27cc44526cdb59271cff719fcaa1249837
Dan Walkes <danwalkes@gmail.com>
2011-01-11 22:28:22 -0700

    Use default phone timezone
    In order to troubleshoot more easily, use the timezone default
    on the phone rather than forcing GMT.

--- microlog4android-core/src/main/java/com/google/code/microlog4android/format/command/DateFormatCommand.java
+++ microlog4android-core/src/main/java/com/google/code/microlog4android/format/command/DateFormatCommand.java
@@ -51,8 +51,7 @@ public class DateFormatCommand implements FormatCommandInterface {
 	final static String[] MONTH_ARRAY = { "JAN", "FEB", "MAR", "APR",
 			"MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
 
-	private final Calendar calendar = Calendar.getInstance(TimeZone
-			.getTimeZone("GMT"));
+	private final Calendar calendar = Calendar.getInstance();
 
 	int format = ABSOLUTE_FORMAT;
 
